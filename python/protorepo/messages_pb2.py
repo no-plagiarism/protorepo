14aa1033d0e23ec402b54410d7960e441b5954aa
d9nich <54186666+d9nchik@users.noreply.github.com>
2022-05-26 19:41:59 +0300

    add full_path

--- python/protorepo/messages_pb2.py
+++ python/protorepo/messages_pb2.py
@@ -14,7 +14,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0emessages.proto\x12\rkafka.message\"K\n\x0e\x41\x64\x64\x46ileRequest\x12\x11\n\tfile_name\x18\x01 \x01(\t\x12\x0c\n\x04hash\x18\x02 \x01(\t\x12\x10\n\x03url\x18\x03 \x01(\tH\x00\x88\x01\x01\x42\x06\n\x04_url\"D\n\tCheckText\x12\x14\n\x0cis_google_on\x18\x01 \x01(\x08\x12\x10\n\x08\x64ocument\x18\x02 \x01(\t\x12\x0f\n\x07user_id\x18\x03 \x01(\x03\x42\x0cZ\n./messagesb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0emessages.proto\x12\rkafka.message\"^\n\x0e\x41\x64\x64\x46ileRequest\x12\x11\n\tfile_name\x18\x01 \x01(\t\x12\x0c\n\x04hash\x18\x02 \x01(\t\x12\x10\n\x03url\x18\x03 \x01(\tH\x00\x88\x01\x01\x12\x11\n\tfull_path\x18\x04 \x01(\tB\x06\n\x04_url\"D\n\tCheckText\x12\x14\n\x0cis_google_on\x18\x01 \x01(\x08\x12\x10\n\x08\x64ocument\x18\x02 \x01(\t\x12\x0f\n\x07user_id\x18\x03 \x01(\x03\x42\x0cZ\n./messagesb\x06proto3')
 
 
 
@@ -39,7 +39,7 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   DESCRIPTOR._options = None
   DESCRIPTOR._serialized_options = b'Z\n./messages'
   _ADDFILEREQUEST._serialized_start=33
-  _ADDFILEREQUEST._serialized_end=108
-  _CHECKTEXT._serialized_start=110
-  _CHECKTEXT._serialized_end=178
+  _ADDFILEREQUEST._serialized_end=127
+  _CHECKTEXT._serialized_start=129
+  _CHECKTEXT._serialized_end=197
 # @@protoc_insertion_point(module_scope)
