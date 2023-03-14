ac3595f3def380cb710576b489eb7da30b625dd2
d9nich <54186666+d9nchik@users.noreply.github.com>
2023-03-14 14:54:10 +0200

    regenerate proto files

--- python/protorepo/messages_pb2.py
+++ python/protorepo/messages_pb2.py
@@ -15,20 +15,21 @@ _sym_db = _symbol_database.Default()
 
 DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0emessages.proto\x12\rkafka.message\"^\n\x0e\x41\x64\x64\x46ileRequest\x12\x11\n\tfile_name\x18\x01 \x01(\t\x12\x0c\n\x04hash\x18\x02 \x01(\t\x12\x10\n\x03url\x18\x03 \x01(\tH\x00\x88\x01\x01\x12\x11\n\tfull_path\x18\x04 \x01(\tB\x06\n\x04_url\"E\n\tCheckText\x12\x14\n\x0cis_google_on\x18\x01 \x01(\x08\x12\x10\n\x08\x64ocument\x18\x02 \x01(\t\x12\x10\n\x08user_uid\x18\x04 \x01(\t\"Y\n\tCheckFile\x12\x11\n\tfile_name\x18\x01 \x01(\t\x12\x10\n\x08user_uid\x18\x02 \x01(\t\x12\x14\n\x0cis_google_on\x18\x03 \x01(\x08\x12\x11\n\tfull_path\x18\x04 \x01(\t\"\xd4\x01\n\x0f\x43heckTextResult\x12@\n\x0c\x63heck_result\x18\x03 \x01(\x0b\x32*.kafka.message.CheckTextResult.CheckResult\x12\x10\n\x08user_uid\x18\x04 \x01(\t\x1am\n\x0b\x43heckResult\x12\x11\n\tfile_hash\x18\x01 \x01(\t\x12\x11\n\tfile_name\x18\x02 \x01(\t\x12\x1d\n\x15plagiarism_percentage\x18\x03 \x01(\x01\x12\x19\n\x11\x65xtended_data_url\x18\x05 \x01(\tB\x0cZ\n./messagesb\x06proto3')
 
-_builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
-_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'messages_pb2', globals())
+_globals = globals()
+_builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
+_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'messages_pb2', _globals)
 if _descriptor._USE_C_DESCRIPTORS == False:
 
   DESCRIPTOR._options = None
   DESCRIPTOR._serialized_options = b'Z\n./messages'
-  _ADDFILEREQUEST._serialized_start=33
-  _ADDFILEREQUEST._serialized_end=127
-  _CHECKTEXT._serialized_start=129
-  _CHECKTEXT._serialized_end=198
-  _CHECKFILE._serialized_start=200
-  _CHECKFILE._serialized_end=289
-  _CHECKTEXTRESULT._serialized_start=292
-  _CHECKTEXTRESULT._serialized_end=504
-  _CHECKTEXTRESULT_CHECKRESULT._serialized_start=395
-  _CHECKTEXTRESULT_CHECKRESULT._serialized_end=504
+  _globals['_ADDFILEREQUEST']._serialized_start=33
+  _globals['_ADDFILEREQUEST']._serialized_end=127
+  _globals['_CHECKTEXT']._serialized_start=129
+  _globals['_CHECKTEXT']._serialized_end=198
+  _globals['_CHECKFILE']._serialized_start=200
+  _globals['_CHECKFILE']._serialized_end=289
+  _globals['_CHECKTEXTRESULT']._serialized_start=292
+  _globals['_CHECKTEXTRESULT']._serialized_end=504
+  _globals['_CHECKTEXTRESULT_CHECKRESULT']._serialized_start=395
+  _globals['_CHECKTEXTRESULT_CHECKRESULT']._serialized_end=504
 # @@protoc_insertion_point(module_scope)
